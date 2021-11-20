7e931e440ba532be869d290b2e56beea94f46db2
martin-brablik <martin.brablik02@upol.cz>
2021-11-20 12:08:59 +0100

    Removed unused code

--- FileExplorer/FileOperations.cs
+++ FileExplorer/FileOperations.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Windows;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace FileExplorer
